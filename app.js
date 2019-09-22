151b194082f93ca7cb775ab5d1fd9b67152db49e
Sergey2311 <sergey556577@gmail.com>
2019-09-23 00:12:02 +0300

    Рабочая версия 2

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
 console.log("Tinkoff");
 
-// regular = 0.01;
-// elevated = 0.05;
-// special = 0.3;
+const purchaseCash = 9999;
+const purchaseType = 'regular';
+const cashback = purchaseType == 'regular' ? total = purchaseCash * 0.01 : 
+    purchaseType == 'elevated' ? total = purchaseCash * 0.05 : total = purchaseCash * 0.3;
+const result = cashback <= 3000 ? Math.trunc(cashback) : 3000;
+console.log(result);
 
-const purchaseCash = 38976;
-let purchaseType = 'regular';
-
-if (purchaseType == 'regular') {
-    total = purchaseCash * 0.01;
-} else { if (purchaseType == 'elevated') {
-    total = purchaseCash * 0.05;
-} else {
-    total = purchaseCash * 0.3; }
-}
-
-
-// финиш - это окончательная сумма 
-// не более 3000
-// тотал - это кэшбек
-
-
-if (total <= 3000) {
-    finish = Math.trunc(total);
-} else {
-    finish = 3000;
-}
-
-console.log(finish);
+// 3 строка - вписываем сумму покупки; 4 строка - выбираем тип покупки;
+// 5-6 строка - высчитываем кэшбек; 7 строка - округляем и приводим к "не более 3000".
