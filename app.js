e184d23735c6a6dc278a585c7f0f0d99bf7ae013
Sergey2311 <sergey556577@gmail.com>
2019-09-22 23:12:18 +0300

    Рабочая версия 1

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("worked");
+console.log("Tinkoff");
+
+// regular = 0.01;
+// elevated = 0.05;
+// special = 0.3;
+
+const purchaseCash = 38976;
+let purchaseType = 'regular';
+
+if (purchaseType == 'regular') {
+    total = purchaseCash * 0.01;
+} else { if (purchaseType == 'elevated') {
+    total = purchaseCash * 0.05;
+} else {
+    total = purchaseCash * 0.3; }
+}
+
+
+// финиш - это окончательная сумма 
+// не более 3000
+// тотал - это кэшбек
+
+
+if (total <= 3000) {
+    finish = Math.trunc(total);
+} else {
+    finish = 3000;
+}
+
+console.log(finish);
