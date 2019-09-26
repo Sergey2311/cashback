fc54bf42e283f655cf12ebd7316c5b4f51b35be4
Sergey2311 <sergey556577@gmail.com>
2019-09-26 22:33:20 +0300

    fixes #5

--- app.js
+++ app.js
@@ -7,5 +7,3 @@ const cashback = purchaseType == 'regular' ? total = purchaseCash * 0.01 :
 const result = cashback <= 3000 ? Math.trunc(cashback) : 3000;
 console.log(result);
 
-// 3 строка - вписываем сумму покупки; 4 строка - выбираем тип покупки;
-// 5-6 строка - высчитываем кэшбек; 7 строка - округляем и приводим к "не более 3000".
