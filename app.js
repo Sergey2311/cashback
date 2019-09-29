9e3b580421769cf25f94ff6dcb14633389b1017a
Sergey2311 <sergey556577@gmail.com>
2019-09-29 20:19:21 +0300

    Итоговый вариант 2

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ console.log("Tinkoff");
 
 const regularCash = 5647; 
 const elevatedCash = 984;
-const specialCash = 20345;
+const specialCash = 205;
 
 const maxCashback = 3000;
 const onePercent = 0.01;
 const fivePercent = 0.05;
 const thirtyPercent = 0.3;
 
-let cashback = regularCash * onePercent + elevatedCash * fivePercent + specialCash * thirtyPercent;
+const cashback = regularCash * onePercent + elevatedCash * fivePercent + specialCash * thirtyPercent;
 
 const result = cashback <= maxCashback ? Math.trunc(cashback) : maxCashback;
 console.log(result + ' рублей');
