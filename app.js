7ee65a521cf8bb4c89c6f3fe2a56791216ac809e
Sergey2311 <sergey556577@gmail.com>
2019-09-29 20:16:46 +0300

    Итоговый вариант

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 console.log("Tinkoff");
 
-const purchaseCash = 21756;
-const purchaseType = 'special';
+const regularCash = 5647; 
+const elevatedCash = 984;
+const specialCash = 20345;
+
 const maxCashback = 3000;
 const onePercent = 0.01;
-const twoPercent = 0.05;
-const threePercent = 0.3;
+const fivePercent = 0.05;
+const thirtyPercent = 0.3;
 
-if (purchaseType == 'regular') {
-    cashback = purchaseCash * onePercent;
-} else { if (purchaseType == 'elevated') {
-    cashback = purchaseCash * twoPercent;
-} else { if (purchaseType == 'special') {
-    cashback = purchaseCash * threePercent; }
-} 
-    }
-
+let cashback = regularCash * onePercent + elevatedCash * fivePercent + specialCash * thirtyPercent;
 
 const result = cashback <= maxCashback ? Math.trunc(cashback) : maxCashback;
-console.log(result);
-
+console.log(result + ' рублей');
